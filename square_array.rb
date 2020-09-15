200386e004fccd3b50b3341f3f2019b337a8be19
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-15 15:44:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  counter = 0
-  array.each do |number|
-    sq_number = number * number
-    array[counter] = sq_number
-    counter += 1
+  # counter = 0
+  # array.each do |number|
+  #   sq_number = number * number
+  #   array[counter] = sq_number
+  #   counter += 1
   end
 end
